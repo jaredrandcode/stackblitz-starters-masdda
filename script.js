5a0cfe722b916ae632362ded018c9ca62f430da8
jaredrandcode <jrand100406@gmail.com>
2024-11-14 12:39:30 -0500

    code thingers

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello!');
+function todaysDate(){
+const d=new Date();
+return "todays date is "+d
+}
+console.log(todaysDate())
+const body = document.body
+const d = new Date();
+if (d.getHours() > 20 || d.getHours()< 8){
+    body.style["backgroundColor"] = "black";
+    body.style["color"] = "white";
+} else {
+    body.style["backgroundColor"] = "white";
+    body.style["color"] = "black";
+}
